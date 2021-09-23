fa3304468e4bd1e91f683c910bab26a67dd2689f
Marco Romanelli <41620979+mromanelli9@users.noreply.github.com>
2021-09-23 10:50:50 +0200

    remove debug code

--- jupyterlab_medialab/dataviz_handlers.py
+++ jupyterlab_medialab/dataviz_handlers.py
@@ -36,16 +36,11 @@ class DataVizAPI(APIHandler):
       
     @staticmethod
     def _parse_user(current_user):
-        # if isinstance(current_user, dict):
-        #     user = current_user['name']
-        # else:
-        #     user = current_user
+        if isinstance(current_user, dict):
+            user = current_user['name']
+        else:
+            user = current_user
         
-        # print('DataVizAPI._parse_user ', user)
-        
-        # return user
-
-        user = 'test'
         return user
 
 
@@ -90,82 +85,3 @@ class GenericAPI(DataVizAPI):
     @staticmethod
     def _get_api_name(path):
         return path.rsplit("/")[-1]
-
-
-# class DataVizAPIWrapper(APIHandler):
-#     API_LIST_GET = ['get_my_visualisations']
-#     API_LIST_POST = ['lock', 'restore', 'delete']
-#     API_LIST = API_LIST_GET + API_LIST_POST
-
-#     @tornado.web.authenticated
-#     def get(self):
-#         api_name = DataVizAPIWrapper._get_api_name(self.request.path)
-
-#         print('DataVizAPIWrapper.GET ', api_name)
-
-#         # check if is the right HTTP method
-#         if not api_name:
-#             raise tornado.web.HTTPError(404)
-#         elif api_name not in DataVizAPIWrapper.API_LIST_GET:
-#             raise tornado.web.HTTPError(405)
-        
-#         self._call_api(api_name)
-
-#     @tornado.web.authenticated
-#     def post(self):
-#         api_name = DataVizAPIWrapper._get_api_name(self.request.path)
-
-#         print('DataVizAPIWrapper.POST ', api_name)
-
-#         # check if is the right HTTP method
-#         if not api_name:
-#             raise tornado.web.HTTPError(404)
-#         elif api_name not in DataVizAPIWrapper.API_LIST_POST:
-#             raise tornado.web.HTTPError(405)
-        
-#         input_data = self.get_json_body()
-#         self._call_api(api_name, **input_data)
-
-#     def _call_api(self, api, *args, **kwargs):
-#         print('DataVizAPIWrapper._call_api ', args, kwargs)
-
-#         user = DataVizAPIWrapper._parse_user(self.current_user)
-
-#         try:
-#             dz = DataViz(user=user)
-#             res = getattr(dz, api)(*args, **kwargs) 
-
-#             print('DataVizAPIWrapper._call_api ', res)
-        
-#         except TypeError as err:
-#             print('DataVizAPIWrapper._call_api exception ', err)
-#             raise tornado.web.HTTPError(400)
-
-#         # Returned when the viz is locked
-#         except DataVizException:
-#             self.finish(json.dumps({
-#                 "data": False
-#             }))
-
-#         # FIXME: add DataVizException on dataviz package to handle this
-#         except PyMongoError as err:
-#             raise tornado.web.HTTPError(500, 'An internal error occured: %s' % err)
-
-#         self.finish(json.dumps({
-#             "data": res
-#         }))
-
-#     @staticmethod
-#     def _get_api_name(path):
-#         return path.rsplit("/")[-1]
- 
-#     @staticmethod
-#     def _parse_user(current_user):
-#         if isinstance(current_user, dict):
-#             user = current_user['name']
-#         else:
-#             user = current_user
-        
-#         print('DataVizAPIWrapper._parse_user ', user)
-
-#         return user
